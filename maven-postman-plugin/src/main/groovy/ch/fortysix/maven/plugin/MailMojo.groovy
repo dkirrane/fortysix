c1eba8023aa1ad2d06c7d73d3cf9b291ddbb6950
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2009-10-30 09:59:35 +0000

    intial load

---
+++ maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/MailMojo.groovy
@@ -0,0 +1,80 @@
+package ch.fortysix.maven.plugin
+
+import org.codehaus.gmaven.mojo.GroovyMojo;
+
+/**
+ * Mojo enabling the conditional sending of email
+ *
+ * @goal send
+ */
+class MailMojo
+    extends GroovyMojo
+{
+    /**
+     * The hello message to display.
+     *
+     * @parameter expression="${message}" default-value="message from maven build"
+     */
+    String message
+
+    /**
+     * Email subject line. 
+     * @parameter expression="${subject}" default-value="maven build"
+     */
+    String subject;
+
+    /**
+     * Email address of sender.
+     * @parameter expression="${from}" 
+     * @required
+     */
+    String from;
+    
+    /**
+     * a list of mail recivers.
+     * @parameter 
+     * @required
+     */
+    List recivers;
+
+    /**
+     * flag to indicate whether to halt the build on any error. The default value is true.
+     * @parameter expression="${failonerror}" default-value="true"
+     */
+    boolean failonerror = true;
+
+    /**
+     * Host name of the SMTP server. The default value is localhost.
+     * @parameter expression="${mailhost}" default-value="localhost"
+     */
+    String mailhost;
+
+    /**
+     * TCP port of the SMTP server. The default value is 25.
+     * @parameter expression="${mailport}" default-value="25"
+     */
+    String mailport;
+
+    void execute() {
+    	log.info("seding mail...")
+    	
+    	
+    	def tolist = ""
+    	recivers.each{ it ->
+    		log.info it
+    		tolist = it+","+tolist
+    	}
+
+    	log.info tolist
+    	
+        ant.mail(from: from, 
+        		subject: subject, 
+        		message: message, 
+        		mailhost: mailhost, 
+        		mailport: mailport, 
+        		failonerror:failonerror,
+        		tolist: tolist){
+
+    	}
+    }
+}
