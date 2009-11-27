3340cb3ba9f65711f158caded8a695c07c954085
domi <domi@schafemachensbesser.ch>
2009-11-27 18:35:37 +0000

    implement support for multiple rules

---
+++ maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/Fileset.groovy
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package ch.fortysix.maven.plugin;
+
+import org.apache.maven.shared.model.fileset.FileSet;
+
+/**
+ * @author Domi
+ *
+ */
+class Fileset extends FileSet {
+
+}
---
+++ maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/Rule.groovy
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ch.fortysix.maven.plugin;
+
+/**
+ * @author Domi
+ *
+ */
+class Rule {
+	String regex
+	boolean positiveTest
+	String[] recivers
+	
+	String toString(){
+		return "[Rule: "+regex+", positiveTest="+positiveTest+", recivers="+recivers+"]"
+	}
+}
