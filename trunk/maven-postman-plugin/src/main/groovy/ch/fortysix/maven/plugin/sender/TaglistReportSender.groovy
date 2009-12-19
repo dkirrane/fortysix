b499522880529fa3337288426c0c4e07b45f73c7
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2009-12-19 17:18:12 +0000

    add TaglistReportSender add SurefireReportSender

---
+++ trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/sender/TaglistReportSender.groovy
@@ -0,0 +1,64 @@
+package ch.fortysix.maven.plugin.sender;
+
+import java.io.File;
+import java.util.Map;
+
+import org.apache.maven.plugin.logging.Log;
+
+import ch.fortysix.maven.plugin.sender.support.TagClassMailContent;
+import ch.fortysix.maven.plugin.sender.support.TaglistReport;
+
+class TaglistReportSender {
+	
+	Log log;
+	
+	TaglistReport taglistReport;
+	
+	Map getMails(File reportFile){
+		def xmlText = reportFile?.text
+		def receiver2Mail = [:]
+		
+		if(xmlText != null){
+			log.info("prepare taglist mails...")
+			def report = new XmlSlurper().parseText(xmlText)
+			
+			// uniquely get all receivers (in a Set)
+			def allReceivers = taglistReport?.tagClasses?.receivers.flatten{} as Set
+			def receiver2DisplayNames = [:]
+			allReceivers.each{ aReceiver -> 
+				def displayNames = taglistReport?.tagClasses?.findAll{ tagClass -> tagClass.receivers.contains(aReceiver) }.displayName
+				log.debug "prepare taglist-mail for $aReceiver with displayNames: $displayNames"
+				receiver2DisplayNames.put(aReceiver, displayNames)
+			}
+			
+			receiver2DisplayNames.each{aReceiver, displayNames ->
+				def tags4Receiver = []
+				displayNames.eachWithIndex { displayName, i ->
+					// the taglist.xml contains only one tag for each tagClass 
+					// (therefore we only use 'find{}' and not 'findAll{}'
+					tags4Receiver << report.tags.tag.find { tag ->
+						def name = tag['@name']
+						log.debug "$name <-> '$displayName'"
+						name == displayName 
+					}				
+				}
+				if(log.isDebugEnabled()){
+					def s = tags4Receiver.size()
+					log.debug "found $s tags for '$aReceiver'"
+				}
+				// assign a mailcontent for each receiver
+				def mailContent = new TagClassMailContent(tagsFromReportFile: tags4Receiver) 
+				receiver2Mail.put(aReceiver, mailContent)
+			}
+		}
+		
+		if(log.isDebugEnabled()){
+			receiver2Mail.each {key, value -> 
+				log.debug value.asMailBody()
+			}
+		}
+		
+		return receiver2Mail
+	}
+	
+}
---
+++ trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/sender/support/PostmanReportMailContent.groovy
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ch.fortysix.maven.plugin.sender.support;
+
+/**
+ * @author Domi
+ *
+ */
+class PostmanReportMailContent {
+	
+	def rules = []
+	
+	String asMailBody(){
+		def body = new StringBuilder()
+		rules.each{	tag -> 
+			body << "\n"
+			body << "Rule: '" << tag.regex << "'"
+			body << "\n"
+			tag.result.eachWithIndex{ result, i ->
+				body << (i+1) << ". file: " << result.file << "\n"
+				result.match.each{
+					body << "\tMatch: " << it << "\n" 
+				}
+			}
+		}
+		
+		return body.toString()
+	}
+}
