1373074c91d8e39fba42861a8d94fc62b3af6ad8
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2009-12-27 08:27:26 +0000

    enhance taglist mail report with cross links

--- trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/report/taglist/TaglistReportBodyGenerator.groovy
+++ trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/report/taglist/TaglistReportBodyGenerator.groovy
@@ -2,16 +2,19 @@
  * 
  */
 package ch.fortysix.maven.report.taglist
-;
 
-import org.apache.maven.doxia.sink.Sink;
+import org.apache.maven.doxia.sink.Sink
 /**
  * Generates the html body for the postman taglist report
  * @author Domi
  */
 class TaglistReportBodyGenerator {
 	
-	def receiver2TestReport
+	def targetTaglistHtmlPage
+	
+	def receiver2TestReport = [:]
+	
+	def tagClass2Ancher = [:]
 	
 	void generateBody(Sink sink){
 		sink.paragraph()
@@ -30,7 +33,24 @@ class TaglistReportBodyGenerator {
 			sink.text receiver
 			sink.tableCell_()
 			sink.tableCell()
-			sink.text report.asMailBody()
+			sink.text "Check out the taglist report: "		
+			sink.lineBreak()
+			report.tagsFromReportFile.each{ tag ->
+				sink.text tag.@count.toString()
+				sink.text " comments for tagClass: "
+				//				sink.bold()
+				//				sink.text tag.@name?.toString()
+				//				sink.bold_()
+				//				sink.paragraph()
+				// create a link (e.g. 'taglist.html#tag_class_1')
+				sink.link targetTaglistHtmlPage+tagClass2Ancher[tag.@name?.toString()]
+				sink.text tag.@name?.toString()
+				sink.link_()
+				sink.lineBreak()
+			}
+			
+			//				report.addToSink(sink)
+			
 			sink.tableCell_()
 			sink.tableRow_()
 		}
