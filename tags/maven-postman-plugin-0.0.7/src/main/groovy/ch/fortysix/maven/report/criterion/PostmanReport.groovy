007d6c400e6fec74c55c3189e8da2f26c0662152
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-01-24 16:24:48 +0000

    [maven-scm] copy for tag maven-postman-plugin-0.0.7

---
+++ tags/maven-postman-plugin-0.0.7/src/main/groovy/ch/fortysix/maven/report/criterion/PostmanReport.groovy
@@ -0,0 +1,21 @@
+package ch.fortysix.maven.report.criterion
+;
+
+import java.io.File;
+
+import ch.fortysix.maven.report.taglist.TagClass;
+
+
+class PostmanReport {
+	
+	boolean skip
+	
+	File reportFile;
+	
+	List tagClasses = []
+	
+	void addTagClasse(TagClass tagClass){
+		tagClasses.add(tagClass)
+	}
+	
+}
---
+++ tags/maven-postman-plugin-0.0.7/src/main/groovy/ch/fortysix/maven/report/surefire/SurefireReportBodyGenerator.groovy
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package ch.fortysix.maven.report.surefire;
+
+import org.apache.maven.doxia.sink.Sink;
+/**
+ * Generates the html body for the postman surefire report
+ * @author Domi
+ *
+ */
+class SurefireReportBodyGenerator {
+	
+	def receiver2TestReport
+	
+	void generateBody(Sink sink){
+		sink.paragraph()
+		sink.text "The reminder mail check the test results was send to this users."
+		sink.table()
+		sink.tableHeaderCell()
+		sink.text "Receiver"
+		sink.tableHeaderCell_()
+		//		sink.tableHeaderCell()
+		//		sink.text "Mail Content"
+		//		sink.tableHeaderCell_()
+		
+		receiver2TestReport.each { receiver, report ->
+			sink.tableRow()
+			sink.tableCell()
+			sink.text receiver
+			sink.tableCell_()
+			sink.tableCell()
+			sink.text receiver
+			sink.tableCell_()
+			sink.tableRow_()
+		}
+		
+		sink.table_()
+		
+		sink.paragraph_()
+	}
+	
+}
