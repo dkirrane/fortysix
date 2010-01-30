fa09420e45fa3ddc3d25f97b2289be68389888da
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-01-30 11:56:00 +0000

    [maven-scm] copy for tag maven-postman-plugin-0.0.7

---
+++ tags/maven-postman-plugin-0.0.7/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/report/taglist/TaglistReport.groovy
@@ -0,0 +1,20 @@
+package ch.fortysix.maven.report.taglist
+;
+
+import java.io.File;
+
+
+
+class TaglistReport {
+	
+	boolean skip
+	
+	File reportFile;
+	
+	List tagClasses = []
+	
+	void addTagClasse(TagClass tagClass){
+		tagClasses.add(tagClass)
+	}
+	
+}
