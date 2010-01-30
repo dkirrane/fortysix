0e01ede3ec55a3f3007c7788ac13069de7501b3d
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-01-30 12:32:59 +0000

    [maven-scm] copy for tag maven-postman-plugin-0.0.7

---
+++ tags/maven-postman-plugin-0.0.7/src/main/groovy/ch/fortysix/maven/report/criterion/PostmanReportMailContent.groovy
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package ch.fortysix.maven.report.criterion
+
+import java.io.File
+
+import org.apache.maven.doxia.sink.Sink;
+
+import ch.fortysix.maven.report.HtmlSnipplet;
+import ch.fortysix.maven.report.SinkSnipplet;
+import ch.fortysix.maven.report.TextSnipplet;
+
+/**
+ * @author Domi
+ *
+ */
+class PostmanReportMailContent implements HtmlSnipplet, TextSnipplet, SinkSnipplet{
+	
+	def rules = []
+	
+	def htmlFragment
+	
+	String html(){
+		htmlFragment
+	}
+	
+	String text(){
+		def body = new StringBuilder()
+		rules.each{	tag -> 
+			body << "\n"
+			body << "Rule: '" << tag.regex << "'"
+			body << "\n"
+			tag.result.eachWithIndex{ result, i ->
+				body << (i+1) << ". file: " << result.file << "\n"
+				result.match.each{
+					body << "\tMatch: " << it << "\n" 
+				}
+			}
+		}
+		
+		body.toString()
+	}
+	
+	void addToSink(Sink sink){
+		sink.table()
+		sink.tableRow()
+		rules.each{ tag ->
+			sink.tableRow()
+			sink.tableCell()
+			sink.text tag.regex.text()
+			tag.result.eachWithIndex{ result, i ->
+				sink.table()
+				sink.tableRow()
+				sink.tableCell()
+				sink.text "$i file: " 
+				sink.text result.file.text() 
+				//				result.match.each{
+				//					body << "\tMatch: " << it << "\n" 
+				//				}
+				sink.tableCell_()
+				sink.tableRow_()
+			}			
+			sink.tableCell_()
+			sink.tableRow_()
+		}
+		sink.table_()
+	}	
+}
