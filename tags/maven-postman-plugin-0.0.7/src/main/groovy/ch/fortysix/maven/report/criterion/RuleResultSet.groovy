4e9f2fb5820d33c21dd0cfe3f2f95893b0cdb527
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-01-30 11:48:16 +0000

    [maven-scm] copy for tag maven-postman-plugin-0.0.7

---
+++ tags/maven-postman-plugin-0.0.7/src/main/groovy/ch/fortysix/maven/report/criterion/RuleResultSet.groovy
@@ -0,0 +1,19 @@
+package ch.fortysix.maven.report.criterion
+;
+
+
+/**
+ * @author Domi
+ *
+ */
+class RuleResultSet {
+	
+	public Rule rule
+	
+	def results = []
+	
+	def addResult(Result result){
+		results.add result
+	}
+	
+}
