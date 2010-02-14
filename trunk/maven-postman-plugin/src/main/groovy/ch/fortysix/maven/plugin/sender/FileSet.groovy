47c8f22e692580b996d48ff4795ba090734a03a4
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-02-14 14:51:33 +0000

    rename the goals disable criterion goal add documentation

--- trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/sender/FileSet.groovy
+++ trunk/maven-postman-plugin/src/main/groovy/ch/fortysix/maven/plugin/sender/FileSet.groovy
@@ -5,12 +5,12 @@ package ch.fortysix.maven.plugin.sender
  * 
  */
 
-import org.apache.maven.shared.model.fileset.FileSet;
+
 
 /**
  * This class has to be in the same package as the plugin using it.
  * @author Domi
  */
-class AttachmentSet extends FileSet {
+class FileSet extends org.apache.maven.shared.model.fileset.FileSet {
 	
 }
