0a8e1cdf4bf434c0bfa22e33445cb6b0ac610989
domi <domi@schafemachensbesser.ch>
2010-05-15 14:05:15 +0000

    initial

---
+++ BuildVerify/src/main/java/ch/fortysix/build/BuildVerify/App.java
@@ -0,0 +1,13 @@
+package ch.fortysix.build.BuildVerify;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
