9111f10075f216a542346725353ee9fd1d67d10a
domi <domi@schafemachensbesser.ch>
2010-05-15 14:28:05 +0000

    [maven-release-plugin](anonymous) copy for tag BuildVerify-0.0.1

--- 46Utilities/src/main/java/ch/fortysix/util/doclet/maker/Maker.java
+++
@@ -1,8 +0,0 @@
-package ch.fortysix.util.doclet.maker;
-
-import com.sun.javadoc.ClassDoc;
-
-
-public interface Maker {
-	public void make(ClassDoc classDoc, String namePostfix, String velocityTemplate);
-}
