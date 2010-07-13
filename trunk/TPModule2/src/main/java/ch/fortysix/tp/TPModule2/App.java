c33b0831a8a3fe689c8930ce969270b34f4c276b
domi@schafemachensbesser.ch <domi@schafemachensbesser.ch@9e868b82-8a76-11de-9d8c-3fbdfc29305b>
2010-07-13 06:37:25 +0000

    initial load

---
+++ trunk/TPModule2/src/main/java/ch/fortysix/tp/TPModule2/App.java
@@ -0,0 +1,13 @@
+package ch.fortysix.tp.TPModule2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! also from: "+ch.fortysix.tp.TPModule1.App.class.getName() );
+    }
+}
